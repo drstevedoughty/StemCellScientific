13bfcb0e40ea5e3975528ad541d71c47610cbd65
Stephen Doughty <drstevedoughty@gmail.com>
2018-07-24 06:50:20 -0600

    moved to bootstrap4

--- index.php
+++ index.php
@@ -5,22 +5,70 @@ session_start();
 <html lang="en">
 <head>
   <title>Stem Cell Scientific</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="styles.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script type="text/javascript"> 
-  
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
    
 </script>
 </head>
-  <body style="background:linear-gradient(15deg, white,BEIGE);" font="face:Calibri">
-
-<?php include 'header.php';?>
-
+  <body style="background:linear-gradient(45deg, #fffff5,#fffff5);" font="face:Calibri">
+ 
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+ 
+    <nav class="navbar navbar-expand-md navbar-dark" style="background-color:#4F84C4">
+	  <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+	     <span class="navbar-toggler-icon"></span>
+	  </button>
+<div class = "collapse navbar-collapse" id="collapse_target">
+	   <ul class="navbar-nav">
+ 	     <li class="nav-item">
+		   <a class="nav-link" href="#">Home</a>
+		 </li>
+	     <li class="nav-item dropdown">
+		   <a class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="dropdown_target" href="#">Services
+		   <span class="caret">
+		   </a>
+		   <div class="dropdown-menu" aria-labelled-by="dropdown_target">
+		   <ul class="navbar-nav">
+		     <a class="dropdown-item">Stem Cell</a>			   
+		     <a class="dropdown-item">Prolotherapy</a>
+			 <a class="dropdown-item">PRP</a>
+           </ul>			 
+		   </div>  
+		 </li>
+ 	     <li class="nav-item">
+		   <a class="nav-link" href="#">Doctor</a>
+		 </li>
+ 	     <li class="nav-item">
+		   <a class="nav-link" href="#">Contract</a>
+		 </li>
+ 	     <li class="nav-item">
+		   <a class="nav-link" href="#">Research</a>
+		 </li>	   	   
+	   </ul>
+	   </div>
+	</nav>
+<br>
+<div class="container-fluid">                                                            
+<!-- Control the column width, and how they should appear on different devices -->      
+<div class="row">                                                                     
+<div class="col-sm-9" >                                                             
+<img src="img/logoz.png"  class="img-fluid" style="max-width:60px">
+<img src="img/stemcellscientificz.png"  class="img-fluid" style="max-width:100%"> 
+</div>                                                                                
+<div class="col-sm-3" align="left" >     
+<br>                                        
+<button type="button" class="btn btn-danger"><img src="img/whitephone.png" width="20px" class="icon">(435)313-0432</button> 
+</div>                                                                                
+</div>                                                                                  
+</div>         	
+	
+	
 <?php if (isset($_SESSION['alert']))
 {  echo "<div id=\"myAlert\" display=\"none\" class=\"alert alert-success alert-dismissible\" role=\"alert\">";
   echo "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>";
@@ -30,20 +78,15 @@ session_start();
 } 
 ?>
   
-<div id="box-one">
-<p>This is box one.</p>
-</div> 
-
-<div id="box-two">
-<p>This is box two.</p>
-</div>
+<br>
 
    
 	<div class="container-fluid">
     <div class="row">
       <div class="col-sm-2" ></div>
 	  <div class="col-sm-8" style="text-align:center" >
-	  <H2>Get Your Life Back</H2>
+	  <H1 style="color:#404040;">Get Your Life Back</H1>
+
 	  <h4>Get out of pain without drugs or surgery</h4>
       <h4>Overcome debilitating illness </h4>
 </div>
@@ -76,7 +119,6 @@ session_start();
     <br>
 	    <br>
 
-     <div class="zzz">
 	 <div class="row">
 	   <div class="col-sm-4" >
 <img class="img-rounded" width="250px" height="auto" src="img/construction.jpg"> <br>  	   
@@ -137,3 +179,4 @@ email: info@stemellscientific.com
 </div>
   </body>
 </html>
+
